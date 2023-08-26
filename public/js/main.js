434efccbae0158c176f5bd595b34b2c282a90b99
Jevgan <ayevhenii@rcit.ukr.education>
2023-08-26 22:37:57 +0300

    font refactored

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function() {
+
+eval("\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n// window.addEventListener('scroll',e =>{\r\n//     let header=document.querySelector('header').classList; \r\n//     let active_class=\"header_scrolled\";\r\n    \r\n//     if(scrollY>100){\r\n//         header.add(active_class); \r\n//     } \r\n//     else{\r\n//         header.remove(active_class)  \r\n//     } \r\n// });     \r\n\r\n// window.addEventListener('scroll', () => {\r\n//     const HEADER = document.querySelector('.header');\r\n//     let animation='';\r\n    \r\n//     if (window.scrollY > 100) {\r\n//         if (animation !== 'normal') {\r\n//             HEADER.style.animation = 'heightFade .2s forwards';\r\n//             animation = 'normal';\r\n//         }\r\n//     } else {\r\n//         if (animation !== 'reverse') {\r\n//             HEADER.style.animation = 'heightFadeReverse .2s forwards';\r\n//             animation = 'reverse';\r\n//         }\r\n//     }\r\n// });\n\n//# sourceURL=webpack://gulp-starter/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
